24cb9bebc27a3741eef4f22870563ce59ea5f89c
CQJTUWilliam <1584075475@qq.com>
2020-01-16 16:42:23 +0800

    create repo

---
+++ cmake-build-debug/CMakeFiles/SortAlgorithms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortAlgorithms.dir/main.cpp.o"
+  "SortAlgorithms.pdb"
+  "SortAlgorithms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SortAlgorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
